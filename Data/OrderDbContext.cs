fd0494989740eccd6819208c324369dc32c43198
Mark Borisov <markmborisov@tu-sofia.bg>
2022-07-20 16:31:50 +0300

    Added OrderModel and MovieModel

--- Data/OrderDbContext.cs
+++ Data/OrderDbContext.cs
@@ -1,33 +1,27 @@
-﻿
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Options;
+﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Cinema
 {
-    internal class UserDbContext : Microsoft.EntityFrameworkCore.DbContext
+    internal class OrderDbContext : Microsoft.EntityFrameworkCore.DbContext
     {
-        
-
-        public UserDbContext()
+        public OrderDbContext()
         {
-            
+
         }
+
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             // connect to mysql with connection string from app settings
-           
+
             options.UseMySQL("Server=localhost;uid=root;pwd=123456789aA;Database=cinema");
         }
 
-        public Microsoft.EntityFrameworkCore.DbSet<User> Users { get; set; }
-
+        public Microsoft.EntityFrameworkCore.DbSet<User> Orders { get; set; }
     }
 }
+
---
+++ Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema
+{
+    
+    internal class Order
+    { 
+        public int id { get; set; }
+        public int user_id { get; set; }
+        public int movie_id { get; set; }
+        public decimal price { get; set; }
+        public int hall_number { get; set; }
+        public string dtp { get; set; }
+    }
+}
